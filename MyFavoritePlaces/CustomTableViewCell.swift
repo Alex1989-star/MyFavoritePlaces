8e22545f5f0ca9c0388f95ca11acb49b4a246b01
Alex1989-star <bio181@yandex.ru>
2021-08-20 12:34:55 +0300

    Added Custom Table View Cell

---
+++ MyFavoritePlaces/CustomTableViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  CustomTableViewCell.swift
+//  MyFavoritePlaces
+//
+//  Created by Леся on 20.08.2021.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+    
+    @IBOutlet var imageOfPlace: UIImageView!
+    
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var locationLabel: UILabel!
+    @IBOutlet var typeLabel: UILabel!
+    
+}
